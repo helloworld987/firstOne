d9083953d402ec8f4783ca0f862049cee6289a30
Madhuri Londhe <londhe.madhuri@gmail.com>
2014-01-23 21:30:09 -0500

    Changes in application

--- src/Application.java
+++ src/Application.java
@@ -8,23 +8,44 @@ public class Application {
 		String fname;
 		String processName;
 		Scanner in;
+		int choice;
 		
 //		System.out.println("Enter config file:");
 //		in = new Scanner(System.in);
 //		fname = in.next();
 //		
 //
-//		System.out.println("Enter process name:");
-//		in = new Scanner(System.in);
-//		processName = in.next();
+		System.out.println("Enter process name:");
+		in = new Scanner(System.in);
+		processName = in.next();
 		
-		Message msg = new Message ("alice", "Ack", "123");
+		//Message msg = new Message ("bob", "Ack", "123");
 		
-		MessagePasser msgPasser = new MessagePasser ("/home/madhuri/DS/Lab0/firstOne/src/test4.yaml", "alice");
+		MessagePasser msgPasser = new MessagePasser ("/home/madhuri/DS/Lab0/firstOne/src/test4.yaml", processName);
 		
-		msgPasser.send(msg);
-		Thread.sleep(5000);
-		System.out.println(msgPasser.receive().getData());
+		System.out.println("1.Send\n"  + "2.Receive\n");
+		
+		while(true) {
+			System.out.println("Enter your choice::");
+			in = new Scanner(System.in);
+			choice = Integer.parseInt(in.next());
+			
+			switch(choice){
+				case 1: Message msg = new Message ("bob", "Ack", "123");
+						msgPasser.send(msg);
+						break;
+						
+				case 2: Message msg1 = msgPasser.receive(); 
+						if( msg1 == null)
+							System.out.println("No message received..!!");
+						else
+							System.out.println(msg1.getData());
+						break;
+			}
+		}
+		//msgPasser.send(msg);
+		//Thread.sleep(5000);
+		
 	}
 
 }
